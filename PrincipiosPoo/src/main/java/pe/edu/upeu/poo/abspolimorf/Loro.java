c984cb8d291bf054816a0d28f12941d9575019cb
JHAMPIER159123 <JHAMPIER159123>
2024-04-08 10:23:40 -0500

    mi primera vez

---
+++ PrincipiosPoo/src/main/java/pe/edu/upeu/poo/abspolimorf/Loro.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.poo.abspolimorf;
+
+/**
+ *
+ * @author Datos
+ */
+public class Loro extends Animal {
+
+    @Override
+    public void animalSound() {
+        System.out.println("Esto es Poliformismo entiende pue");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Esta clase no me gusta me da Zzz zz...");
+    }
+
+}
---
+++ PrincipiosPoo/src/main/java/pe/edu/upeu/poo/poliformismo/Loro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.poo.poliformismo;
+
+/**
+ *
+ * @author Datos
+ */
+public class Loro extends Animal {
+
+    //Aplicacion de concepto de polimorfismo
+    @Override
+    public void sonidoAnimal() {
+        System.out.println("Hola Manito Aprende ... aprende ... esfuerzate");
+    }
+}
